e293edd3e5f08f4431666bdd1469606eb8c8e42b
Nitin Yadav <nitinyadav.8ti.08@gmail.com>
2026-03-26 17:46:03 +0530

    Remove unused components, assets, and configuration files from the frontend
    - Deleted icons.svg, App.jsx, hero.png, react.svg, vite.svg, Asidebar.jsx, Header.jsx, index.css, main.jsx, and various page components (Academic, Classes, Dashboard, FeeManagements, FeePayments, Sections, Students, StudentsEnroll).
    - Removed Vite configuration file.

--- frontend/src/pages/FeeManagements.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const FeeManagements = () => {
-  return (
-    <div>FeeManagements</div>
-  )
-}
-
-export default FeeManagements
--- frontend/src/pages/FeePayments.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const FeePayments = () => {
-  return (
-    <div>FeePayments</div>
-  )
-}
-
-export default FeePayments
